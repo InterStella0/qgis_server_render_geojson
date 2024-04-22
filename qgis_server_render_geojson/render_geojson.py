575f6b73541c930b2483c180461cd980a8584993
Stella <sarahprog667@gmail.com>
2024-04-22 16:14:40 +0800

    micro-optimize

--- qgis_server_render_geojson/render_geojson.py
+++ qgis_server_render_geojson/render_geojson.py
@@ -38,33 +38,27 @@ class ParameterError(Exception):
     pass
 
 
-class RenderGeojsonFilter(QgsServerFilter):
+DPI_CONST = 39.37
 
+class RenderGeojsonFilter(QgsServerFilter):
     def __init__(self, serverIface=None):
         if serverIface:
             super().__init__(serverIface)
 
         self.prefix_path = os.environ.get('QGIS_RENDERGEOJSON_PREFIX')
 
-    def requestReady(self):
-        pass
-
-    def sendResponse(self):
-        pass
-
     def _resolve_url(self, url):
         """If the path exists locally, relative to the prefix path, return this. If not, try downloading the file."""
 
+        local_path = None
         if self.prefix_path:
             local_path = os.path.join(self.prefix_path, url)
-        else:
-            local_path = None
 
         if not local_path or not os.path.exists(local_path):
             try:
-                local_path, headers = urllib.request.urlretrieve(url)
+                local_path, _ = urllib.request.urlretrieve(url)
             except ValueError:
-                raise ParameterError('The file `{}` could not be found locally and not be retrieved as download.'.format(url))
+                raise ParameterError(f'The file `{url}` could not be found locally and not be retrieved as download.')
 
         return local_path
 
@@ -77,101 +71,105 @@ class RenderGeojsonFilter(QgsServerFilter):
         rw_context = QgsReadWriteContext()
         layer.readStyle(doc, '', rw_context)
 
-    def responseComplete(self):
-        request = self.serverInterface().requestHandler()
+    def handle_requests(self, request):
         params = request.parameterMap()
 
+        try:
+            geojson = params['GEOJSON']
+        except KeyError:
+            raise ParameterError('Parameter GEOJSON must be set.')
+
+        try:
+            style = params['STYLE']
+        except KeyError:
+            raise ParameterError('Parameter STYLE must be set.')
+
+        try:
+            width = int(params['WIDTH'])
+        except (KeyError, TypeError):
+            raise ParameterError('Parameter WIDTH must be integer.')
+        try:
+            height = int(params['HEIGHT'])
+        except (KeyError, TypeError):
+            raise ParameterError('Parameter HEIGHT must be integer.')
+
+        try:
+            dpi = int(params['DPI'])
+        except TypeError:
+            raise ParameterError('Parameter DPI must be integer.')
+        except KeyError:
+            dpi = 96
+
+        try:
+            minx, miny, maxx, maxy = params.get('BBOX').split(',')
+            bbox = QgsRectangle(float(minx), float(miny), float(maxx), float(maxy))
+        except (ValueError, AttributeError):
+            raise ParameterError('Parameter BBOX must be specified in the form `min_x,min_y,max_x,max_y`.')
+
+        _resolver = self._resolve_url
+        geojson_file_name = _resolver(geojson)
+
+        if '$type' in style:
+            polygon_style = _resolver(style.replace('$type', 'polygons'))
+            line_style = _resolver(style.replace('$type', 'lines'))
+            point_style = _resolver(style.replace('$type', 'points'))
+        else:
+            point_style = line_style = polygon_style = _resolver(style)
+
+        _style_loader = self._load_style
+        polygon_layer = QgsVectorLayer(geojson_file_name + '|geometrytype=Polygon', 'polygons', 'ogr')
+        _style_loader(polygon_layer, polygon_style)
+        line_layer = QgsVectorLayer(geojson_file_name + '|geometrytype=Line', 'lines', 'ogr')
+        _style_loader(line_layer, line_style)
+        point_layer = QgsVectorLayer(geojson_file_name + '|geometrytype=Point', 'points', 'ogr')
+        _style_loader(point_layer, point_style)
+
+        settings = QgsMapSettings()
+        settings.setOutputSize(QSize(width, height))
+        settings.setOutputDpi(dpi)
+        settings.setExtent(bbox)
+        settings.setLayers([polygon_layer, line_layer, point_layer])
+        settings.setBackgroundColor(QColor(Qt.transparent))
+        renderer = QgsMapRendererParallelJob(settings)
+
+        event_loop = QEventLoop()
+        renderer.finished.connect(event_loop.quit)
+        renderer.start()
+
+        event_loop.exec_()
+
+        img = renderer.renderedImage()
+        img.setDotsPerMeterX(dpi * DPI_CONST)
+        img.setDotsPerMeterY(dpi * DPI_CONST)
+        image_data = QByteArray()
+        buf = QBuffer(image_data)
+        buf.open(QIODevice.WriteOnly)
+        img.save(buf, 'PNG')
+
+        request.setResponseHeader('Content-type', 'image/png')
+        request.appendBody(image_data)
+
+    def responseComplete(self):
+        request = self.serverInterface().requestHandler()
+
         # SERVICE=RENDERGEOJSON -- we are taking over
-        if params.get('SERVICE', '').upper() == 'RENDERGEOJSON':
-            request.clear()
-            try:
-                # Parse parameters
-                geojson = params.get('GEOJSON')
-                if not geojson:
-                    raise ParameterError('Parameter GEOJSON must be set.')
+        if request.parameterMap().get('SERVICE', '').upper() != 'RENDERGEOJSON':
+            return
 
-                style = params.get('STYLE')
-                if not style:
-                    raise ParameterError('Parameter STYLE must be set.')
-
-                try:
-                    width = int(params.get('WIDTH'))
-                except TypeError:
-                    raise ParameterError('Parameter WIDTH must be integer.')
-                try:
-                    height = int(params.get('HEIGHT'))
-                except TypeError:
-                    raise ParameterError('Parameter HEIGHT must be integer.')
-
-                try:
-                    dpi = int(params.get('DPI', 96))
-                except TypeError:
-                    raise ParameterError('Parameter DPI must be integer.')
-
-                try:
-                    minx, miny, maxx, maxy = params.get('BBOX').split(',')
-                    bbox = QgsRectangle(float(minx), float(miny), float(maxx), float(maxy))
-                except (ValueError, AttributeError):
-                    raise ParameterError('Parameter BBOX must be specified in the form `min_x,min_y,max_x,max_y`.')
-
-                geojson_file_name = self._resolve_url(geojson)
-
-                if '$type' in style:
-                    polygon_style = self._resolve_url(style.replace('$type', 'polygons'))
-                    line_style = self._resolve_url(style.replace('$type', 'lines'))
-                    point_style = self._resolve_url(style.replace('$type', 'points'))
-                else:
-                    polygon_style = self._resolve_url(style)
-                    line_style = polygon_style
-                    point_style = polygon_style
-
-                polygon_layer = QgsVectorLayer(
-                    geojson_file_name + '|geometrytype=Polygon', 'polygons', 'ogr')
-                self._load_style(polygon_layer, polygon_style)
-                line_layer = QgsVectorLayer(
-                    geojson_file_name + '|geometrytype=Line', 'lines', 'ogr')
-                self._load_style(line_layer, line_style)
-                point_layer = QgsVectorLayer(
-                    geojson_file_name + '|geometrytype=Point', 'points', 'ogr')
-                self._load_style(point_layer, point_style)
-
-                settings = QgsMapSettings()
-                settings.setOutputSize(QSize(width, height))
-                settings.setOutputDpi(dpi)
-                settings.setExtent(bbox)
-                settings.setLayers([polygon_layer, line_layer, point_layer])
-                settings.setBackgroundColor(QColor(Qt.transparent))
-                renderer = QgsMapRendererParallelJob(settings)
-
-                event_loop = QEventLoop()
-                renderer.finished.connect(event_loop.quit)
-                renderer.start()
-
-                event_loop.exec_()
-
-                img = renderer.renderedImage()
-                img.setDotsPerMeterX(dpi * 39.37)
-                img.setDotsPerMeterY(dpi * 39.37)
-                image_data = QByteArray()
-                buf = QBuffer(image_data)
-                buf.open(QIODevice.WriteOnly)
-                img.save(buf, 'PNG')
-
-                request.setResponseHeader('Content-type', 'image/png')
-                request.appendBody(image_data)
-            except ParameterError as e:
-                QgsMessageLog.logMessage(
-                    "RenderGeojson.responseComplete :: ParameterError")
-                request.setResponseHeader('Content-type', 'text/plain')
+        request.clear()
+        try:
+            self.handle_requests(request)
+        except Exception as e:
+            QgsMessageLog.logMessage(f"RenderGeojson.responseComplete :: {type(e).__name__}")
+            request.setResponseHeader('Content-type', 'text/plain')
+            if isinstance(e, ParameterError):
                 request.appendBody(str(e).encode('utf-8'))
-            except:
-                QgsMessageLog.logMessage(
-                    "RenderGeojson.responseComplete :: Exception")
-                QgsMessageLog.logMessage(
-                    "RenderGeojson.responseComplete ::   {}".format(traceback.format_exc()))
-                request.setResponseHeader('Content-type', 'text/plain')
-                request.appendBody(b'Unhandled error')
-                request.appendBody(traceback.format_exc().encode('utf-8'))
+                return
+
+            err_trace = traceback.format_exc()
+            QgsMessageLog.logMessage(f"RenderGeojson.responseComplete ::  {err_trace}")
+            request.appendBody(b'Unhandled error')
+            request.appendBody(err_trace.encode('utf-8'))
 
 
 class RenderGeojsonServer:
